7398d730ef4d16e7baf045a1c327bf305c579192
henrique <1100924@isep.ipp.pt>
2016-12-15 14:02:04 +0000

    ui accordion

--- Floweek-Angular/public_html/js/directives/uiBestPlayerMissingDirective.js
+++ Floweek-Angular/public_html/js/directives/uiBestPlayerMissingDirective.js
@@ -1,25 +1,34 @@
-angular.module("Floweek").directive("uiBestPlayerMissing", function() {
+angular.module("Floweek").directive("uiBestPlayerMissing", function($filter) {
     return {
-        
         require: "ngModel",
-        
         link: function(scope, element, attrs, ngModel) {
-            
-            var _formatName = function(name){
-              
-                name = name.replace(/[^\w]+/g, "");
-                
+
+            var _formatName = function(name) {
+
+                name = name.replace(/[^a-zA-Z-]+/g, "");
+
                 return name;
-                
+
             };
 
             element.bind("keyup", function() {
-                
+
                 ngModel.$setViewValue(_formatName(ngModel.$viewValue));
-                
+
                 ngModel.$render();
-                
+
             });
+            
+            // Pass value to scope only when size > 2
+
+            ngModel.$parsers.push(function(value) {
+
+                if (value.length > 2) {
+                    return value;
+                }
+
+            });
+
         }
     };
 });
