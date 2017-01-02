95b883589e2849fed3f3d14dfca2d4f4082f7c05
henrique <1100924@isep.ipp.pt>
2017-01-02 14:00:01 +0000

    route 404

--- Floweek-Angular/public_html/js/config/routeConfig.js
+++ Floweek-Angular/public_html/js/config/routeConfig.js
@@ -1,8 +1,14 @@
 angular.module("Floweek").config(function($routeProvider) {
+
     $routeProvider.when("/", {
         templateUrl: "view/home/index.html",
         controller: "homeController"
     });
+
+    $routeProvider.when("/error", {
+        templateUrl: "view/error/404.html"
+    });
+
     $routeProvider.otherwise({redirectTo: "/"});
 });
 
--- Floweek-Angular/public_html/js/interceptors/timestampsInterceptor.js
+++ Floweek-Angular/public_html/js/interceptors/timestampsInterceptor.js
@@ -1,8 +1,21 @@
 angular.module("Floweek").factory("timestampInterceptor", function() {
 
     return {
+        
         request: function(config) {
-            console.log(config.url);
+
+            var url = config.url;
+
+            if (url.indexOf('view') > -1) {
+                return config;
+            }
+
+            var time = new Date().getTime();
+
+            var newUrl = config.url + '?timestamps=' + time;
+            
+            console.log(newUrl);
+            
             return config;
         }
     };
